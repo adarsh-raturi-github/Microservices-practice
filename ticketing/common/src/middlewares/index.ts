ffc0fb554392740ce8017102c25b603bbcc93679
adarsh raturi <adarshraturi@MacBookAir.lan>
2026-06-18 18:17:56 +0530

    Update package code

--- ticketing/common/src/middlewares/index.ts
+++ ticketing/common/src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export * from "./error-handler.middleware";
 export * from "./validate-request.middleware";
-export * from "./current-user.middleware"
+export * from "./current-user.middleware";
+export * from "./not-authorize.middleware";
