9be20124519c06b6d886475bc69f7fb577374f91
adarsh raturi <adarshraturi@MacBookAir.lan>
2026-06-18 19:45:42 +0530

    Update package code

--- ticketing/common/src/errors/index.ts
+++ ticketing/common/src/errors/index.ts
@@ -2,3 +2,4 @@ export * from "./database-connection.error";
 export * from "./request-validator.error";
 export * from "./bad-request.error";
 export * from "./no-authenticate.error";
+export * from "./no-found-error";
